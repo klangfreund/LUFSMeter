1d70c21f52ceb495b5f855b1315097d4d651ab84
Samuel <sg@klangfreund.com>
2018-04-29 11:52:46 +0200

    Changed the license from GPL2 to MIT.

--- README.rst
+++ README.rst
@@ -7,10 +7,34 @@ R128 and other international loudness standards.
 
 It is available at https://www.klangfreund.com/lufsmeter/ .
 
-The core measurement implementation is published here, under the GPL2 license.
+The core measurement implementation is published here, under the MIT license.
 
 Dependencies
 ------------
 
-The excellent JUCE framework (http://www.juce.com).
+The excellent JUCE framework (https://github.com/WeAreROLI/JUCE).
+
+
+The MIT License (MIT)
+---------------------
+
+Copyright (c) 2011-2018 Klangfreund Samuel Gaehwiler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
--- filters/SecondOrderIIRFilter.h
+++ filters/SecondOrderIIRFilter.h
@@ -4,18 +4,37 @@
  SecondOrderIIRFilter.h
  
  
- This file is part of the LUFS Meter audio measurement plugin.
- Copyright 2011-2016 by Klangfreund, Samuel Gaehwiler.
+ By Samuel Gaehwiler from Klangfreund.
+ Used in the klangfreund.com/lufsmeter/
+
+ License: MIT
+
+ I'd be happy to hear about your usage of this code!
+ -> klangfreund.com/contact/
  
  -------------------------------------------------------------------------------
  
- The LUFS Meter can be redistributed and/or modified under the terms of the GNU 
- General Public License Version 2, as published by the Free Software Foundation.
- A copy of the license is included with these source files. It can also be found
- at www.gnu.org/licenses.
+ The MIT License (MIT)
  
- The LUFS Meter is distributed WITHOUT ANY WARRANTY.
- See the GNU General Public License for more details.
+ Copyright (c) 2018 Klangfreund, Samuel Gaehwiler
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
  
  -------------------------------------------------------------------------------
  
